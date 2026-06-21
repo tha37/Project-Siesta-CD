91cb7e313578337934c9887c860f57f3d09312fd
Thiha <130574634+tha37@users.noreply.github.com>
2026-06-21 22:40:17 +0700

    Update message.py
    fix msg copy

--- bot/helpers/message.py
+++ bot/helpers/message.py
@@ -1,93 +1,218 @@
 import os
 import asyncio
+
 from pyrogram.types import Message
 from pyrogram.errors import MessageNotModified, FloodWait
+
 from bot.tgclient import aio
 from bot.settings import bot_set
 from bot.logger import LOGGER
 
+
 current_user = []
+
 user_details = {
-    'user_id': None, 'name': None, 'user_name': None, 
-    'r_id': None, 'chat_id': None, 'provider': None,
-    'bot_msg': None, 'link': None, 'override' : None
+    'user_id': None,
+    'name': None, # Name of the user 
+    'user_name': None, # Username of the user
+    'r_id': None, # Reply to message id
+    'chat_id': None,
+    'provider': None,
+    'bot_msg': None,
+    'link': None,
+    'override' : None # To skip checking media exist
 }
 
+
 async def fetch_user_details(msg: Message, reply=False) -> dict:
+    """
+    args:
+        msg - pyrogram Message()
+        reply - if user message was reply to another message
+    """
     details = user_details.copy()
+
     details['user_id'] = msg.from_user.id
     details['name'] = msg.from_user.first_name
-    details['user_name'] = msg.from_user.username if msg.from_user.username else msg.from_user.mention()
+    if msg.from_user.username:
+        details['user_name'] = msg.from_user.username
+    else:
+        details['user_name'] = msg.from_user.mention()
     details['r_id'] = msg.reply_to_message.id if reply else msg.id
     details['chat_id'] = msg.chat.id
-    try: details['bot_msg'] = msg.id
-    except: pass
+    try:
+        details['bot_msg'] = msg.id
+    except:
+        pass
     return details
 
-async def check_user(uid=None, msg=None, restricted=False) -> bool:
-    # Admin access
-    if uid in bot_set.admins or (msg and msg.from_user and msg.from_user.id in bot_set.admins):
-        return True
-    
-    # Group Access Check
-    if msg and msg.chat.type in ['group', 'supergroup']:
-        return True
-    
-    # Public access check
-    if bot_set.bot_public:
-        return True
-        
-    return False
 
-async def antiSpam(uid=None, cid=None, revoke=False) -> bool:
-    if revoke:
-        if bot_set.anti_spam == 'CHAT+':
-            if cid in current_user: current_user.remove(cid)
-        elif bot_set.anti_spam == 'USER':
-            if uid in current_user: current_user.remove(uid)
-    else:
-        if bot_set.anti_spam == 'CHAT+':
-            if cid in current_user: return True
-            else: current_user.append(cid)
-        elif bot_set.anti_spam == 'USER':
-            if uid in current_user: return True
-            else: current_user.append(uid)
+async def check_user(uid=None, msg=None, restricted=False) -> bool:
+    """
+    Args:
+        uid - User ID (only needed for restricted access)
+        msg - Pyrogram Message (for getting chatid and userid)
+        restricted - Access only to admins (bool)
+    Returns:
+        True - Can access
+        False - Cannot Access 
+    """
+    user_id = uid
+    if msg and msg.from_user:
+        user_id = msg.from_user.id
+
+    # ၁။ Admin ဖြစ်ပါက အမြဲတမ်း သုံးခွင့်ပြုမည်
+    if user_id in bot_set.admins:
+        return True
+
+    if restricted:
         return False
 
-async def send_message(user, item, itype='text', caption=None, markup=None, chat_id=None, meta=None):
+    # ၂။ Group Access Logic (Bot ကို Public ဖွင့်ထားသော်လည်း Group မှလွဲ၍ Private Chat တွင် Admin မှတစ်ပါး မည်သူမျှ သုံးခွင့်မပြုပါ)
+    if msg and msg.chat:
+        chat_type = str(msg.chat.type).lower()
+        is_group = "group" in chat_type or "supergroup" in chat_type
+
+        if is_group:
+            if bot_set.bot_public:
+                return True
+            else:
+                all_chats = list(bot_set.admins) + bot_set.auth_chats + bot_set.auth_users 
+                if user_id in all_chats or msg.chat.id in all_chats:
+                    return True
+        else:
+            # Group မဟုတ်ပါက (Private Chat/DM ဖြစ်ပါက) Admin မှလွဲ၍ အခြားသူများကို သုံးခွင့်မပြုပါ
+            return False
+
+    # Fallback (msg မပါလာပါက စစ်ဆေးရန် logic)
+    if bot_set.bot_public:
+        return True
+    else:
+        all_chats = list(bot_set.admins) + bot_set.auth_chats + bot_set.auth_users
+        if user_id in all_chats:
+            return True
+
+    return False
+
+
+async def antiSpam(uid=None, cid=None, revoke=False) -> bool:
+    """
+    Checks if user/chat in waiting mode(anti spam)
+    Args
+        uid: User id (int)
+        cid: Chat id (int)
+        revoke: bool (if to revoke the given ID)
+    Returns:
+        True - if spam
+        False - if not spam
+    """
+    if revoke:
+        if bot_set.anti_spam == 'CHAT+':
+            if cid in current_user:
+                current_user.remove(cid)
+        elif bot_set.anti_spam == 'USER':
+            if uid in current_user:
+                current_user.remove(uid)
+    else:
+        if bot_set.anti_spam == 'CHAT+':
+            if cid in current_user:
+                return True
+            else:
+                current_user.append(cid)
+        elif bot_set.anti_spam == 'USER':
+            if uid in current_user:
+                return True
+            else:
+                current_user.append(uid)
+        return False
+
+
+async def send_message(user, item, itype='text', caption=None, markup=None, chat_id=None, \
+        meta=None):
+    """
+    user: user details (dict)
+    item: to send
+    itype: pic|doc|text|audio (str)
+    caption: text
+    markup: buttons
+    chat_id: if override chat from user details
+    thumb: thumbnail for sending audio
+    meta: metadata for the audio file
+    """
     if not isinstance(user, dict):
         user = await fetch_user_details(user)
-    target_chat = chat_id if chat_id else user['chat_id']
+    chat_id = chat_id if chat_id else user['chat_id']
 
+    msg = None
     try:
         if itype == 'text':
-            msg = await aio.send_message(chat_id=target_chat, text=item, reply_to_message_id=user['r_id'], reply_markup=markup, disable_web_page_preview=True)
+            msg = await aio.send_message(
+                chat_id=chat_id,
+                text=item,
+                reply_to_message_id=user['r_id'],
+                reply_markup=markup,
+                disable_web_page_preview=True
+            )
+            
         elif itype == 'doc':
-            msg = await aio.send_document(chat_id=target_chat, document=item, caption=caption, reply_to_message_id=user['r_id'])
+            msg = await aio.send_document(
+                chat_id=chat_id,
+                document=item,
+                caption=caption,
+                reply_to_message_id=user['r_id']
+            )
+
         elif itype == 'audio':
-            msg = await aio.send_audio(chat_id=target_chat, audio=item, caption=caption, duration=int(meta['duration']), performer=meta['artist'], title=meta['title'], thumb=meta['thumbnail'], reply_to_message_id=user['r_id'])
+            msg = await aio.send_audio(
+                chat_id=chat_id,
+                audio=item,
+                caption=caption,
+                duration=int(meta['duration']),
+                performer=meta['artist'],
+                title=meta['title'],
+                thumb=meta['thumbnail'],
+                reply_to_message_id=user['r_id']
+            )
+
         elif itype == 'pic':
-            msg = await aio.send_photo(chat_id=target_chat, photo=item, caption=caption, reply_to_message_id=user['r_id'])
-        
-        # Channel Dump Logic
-        if bot_set.dump_channel and itype in ['audio', 'doc']:
+            msg = await aio.send_photo(
+                chat_id=chat_id,
+                photo=item,
+                caption=caption,
+                reply_to_message_id=user['r_id']
+            )
+
+        # Channel Dump Logic (copy_message ကို အသုံးပြု၍ မူရင်း logic မပျက်စေဘဲ dump လုပ်ပေးပါသည်)
+        if msg and bot_set.dump_channel and itype in ['audio', 'doc']:
             try:
-                await aio.send_copy(chat_id=bot_set.dump_channel, from_chat_id=msg.chat.id, message_id=msg.id)
+                await aio.copy_message(
+                    chat_id=bot_set.dump_channel,
+                    from_chat_id=msg.chat.id,
+                    message_id=msg.id
+                )
             except Exception as e:
                 LOGGER.error(f"Channel Dump Error: {e}")
 
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await send_message(user, item, itype, caption, markup, chat_id, meta)
+
     return msg
+
 
 async def edit_message(msg:Message, text, markup=None, antiflood=True):
     try:
-        return await msg.edit_text(text=text, reply_markup=markup, disable_web_page_preview=True)
+        edited = await msg.edit_text(
+            text=text,
+            reply_markup=markup,
+            disable_web_page_preview=True
+        )
+        return edited
     except MessageNotModified:
         return None
     except FloodWait as e:
         if antiflood:
             await asyncio.sleep(e.value)
             return await edit_message(msg, text, markup, antiflood)
-        return None
+        else:
+            return None
